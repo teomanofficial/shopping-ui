44924a174cf1d56e966f5cb8b30fb5c8480615c5
hsntngr <hasantingir@piton.com.tr>
2022-08-24 15:37:27 +0300

    add home component and provide overall information

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', loadChildren: () => import('./modules/home/home.module').then(x => x.HomeModule) },
+  { path: 'orders', loadChildren: () => import('./modules/orders/orders.module').then(x => x.OrdersModule) },
+  { path: 'products', loadChildren: () => import('./modules/products/products.module').then(x => x.ProductsModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
